9fbe0a49964fe438c31328013aed18fc5fb27440
nayan888000 <57991786+nayan888000@users.noreply.github.com>
2020-08-08 23:51:01 +0530

    Update product_provider.dart

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -6,38 +6,7 @@ import 'dart:convert';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    /*Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),*/
+    
   ];
 
   //var _showFavoritesOnly=false;
@@ -77,7 +46,7 @@ class Products with ChangeNotifier {
     final filterString =
         filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url =
-        'https://shopapp-77921.firebaseio.com/products.json?auth=$authToken&$filterString';
+        '';
     try {
       final response = await http.get(url);
       final extractData = json.decode(response.body) as Map<String, dynamic>;
@@ -86,7 +55,7 @@ class Products with ChangeNotifier {
         return;
       }
       url =
-          'https://shopapp-77921.firebaseio.com/userFavorites/$userId.json?auth=$authToken';
+          '';
       final favoriteResponse = await http.get(url);
       final favoriteData = json.decode(favoriteResponse.body);
 
@@ -110,7 +79,7 @@ class Products with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final url =
-        'https://shopapp-77921.firebaseio.com/products.json?auth=$authToken';
+        '';
     try {
       final response = await http.post(
         url,
@@ -143,7 +112,7 @@ class Products with ChangeNotifier {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       final url =
-          'https://shopapp-77921.firebaseio.com/products/$id.json?auth=$authToken';
+          '';
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -160,7 +129,7 @@ class Products with ChangeNotifier {
 
   Future<void> deleteProduct(String id) async {
     final url =
-        'https://shopapp-77921.firebaseio.com/products/$id.json?auth=$authToken';
+        '';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
     _items.removeWhere((prod) => prod.id == id);
